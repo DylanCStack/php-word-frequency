f00ce5b62fc1dc50c5f4014d54465505346fffa0
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-02-17 10:04:44 -0800

    add routing for input and results display

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     date_default_timezone_set("America/Los_Angeles");
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/WordFrequency.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
 
     $app = new Silex\Application();
 
@@ -12,7 +12,13 @@
     $app['debug'] = true;
 
     $app->get("/", function() use ($app) {
-        return "sample root page";
+        return $app['twig']->render("input.html.twig", array("matches" => null, "input1"=>null, "input2"=>null));
+    });
+
+    $app->post("/results", function() use ($app) {
+        $counter = new RepeatCounter;
+        $result = $counter->CountRepeats($_POST['search_with'], $_POST['search_through']);
+        return $app['twig']->render("results.html.twig",array("matches" => $result, "input1"=>$_POST['search_with'], "input2"=>$_POST['search_through']));
     });
 
     return $app;
