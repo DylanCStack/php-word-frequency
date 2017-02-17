ed8d2f1c21dcb6a71127279b2e63023381b73aa3
Dylan Stackhouse <stackhouse.dylan@gmail.com>
2017-02-17 08:56:40 -0800

    pass first test/spec

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,7 +3,13 @@
     class RepeatCounter{
         function CountRepeats($search_with, $search_through)
         {
-            
+            for($i=0; $i<strlen($search_through); $i++)
+            {
+                if($search_with[0]==$search_through[$i])
+                {
+                    return 1;
+                }
+            }
         }
     }
 ?>
